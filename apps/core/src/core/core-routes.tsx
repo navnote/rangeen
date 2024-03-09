6a432f385d8fb5b49919d88699f9af6ec0cfafef
Navjot Ahuja <navjotahuja92@gmail.com>
2024-03-10 00:15:40 +0100

    Cleanup and Base URL

--- apps/core/src/core/core-routes.tsx
+++ apps/core/src/core/core-routes.tsx
@@ -1,13 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom';
 import { CoreLayout } from './core-layout';
 import { HomePage } from '../components/home';
 
-const router = createBrowserRouter([
+const appRoutes: RouteObject[] = [
   {
     element: <CoreLayout />,
     children: [{ path: '/', element: <HomePage /> }],
   },
-]);
+];
+
+const router = createBrowserRouter([...appRoutes], {
+  basename: import.meta.env.BASE_URL,
+});
 
 export function CoreRoutes() {
   return <RouterProvider router={router} />;
